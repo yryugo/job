7c1fecf825113a20b8ef2aa0cdcb09607ae6b253
yryugo <yryugo@sejuku.net>
2018-11-13 16:42:04 +0900

    関数ファイルを作成

---
+++ setting/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function connectDb() {
+  try {
+    return new PDO(DSN, DB_USER, DB_PASSWORD);
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+  }
+}
+
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, "UTF-8");
+}
